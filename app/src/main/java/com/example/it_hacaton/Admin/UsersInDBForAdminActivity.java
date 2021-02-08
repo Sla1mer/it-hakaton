1f00be912b7c97bee4f58626b34e67c850f578c3
frishart500 <63844334+frishart500@users.noreply.github.com>
2021-02-09 00:41:56 +0200

    end_autoCompleteListsner

--- app/src/main/java/com/example/it_hacaton/Admin/UsersInDBForAdminActivity.java
+++ app/src/main/java/com/example/it_hacaton/Admin/UsersInDBForAdminActivity.java
@@ -6,7 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.it_hacaton.API.ApiClient;
@@ -27,6 +30,7 @@ import retrofit2.Response;
 
 public class UsersInDBForAdminActivity extends AppCompatActivity {
     private RecyclerView rv;
+    private EditText search;
     private ApiInterface apiInterface;
     private ApiInterface apiInterface2;
  private ImageView addImage;
@@ -39,6 +43,24 @@ public class UsersInDBForAdminActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         String name_db = intent.getStringExtra("name_db");
+
+        search = findViewById(R.id.search);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
 
         apiInterface = ApiClient.getAppClient().create(ApiInterface.class);
         Call<List<GetPersonFromDBPersonal>> call = apiInterface.get_list_db_personal(name_db);
@@ -73,4 +95,15 @@ public class UsersInDBForAdminActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this));//
 
     }
+
+    private void filter(String text){
+        ArrayList<ItemUsersForAdmin> array = new ArrayList<>();
+        for(ItemUsersForAdmin item : arrayList){
+            if(item.getName().toLowerCase().contains(text.toLowerCase()) || item.getMiddleName().toLowerCase().contains(text.toLowerCase()) || item.getLastName().toLowerCase().contains(text.toLowerCase())){
+                array.add(item);
+            }
+        }
+        adapter.filterList(array);
+    }
+
 }
